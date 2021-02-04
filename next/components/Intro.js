25edf0ced5ab50446c2d753b85234b33130e089f
james <jguest777@gmail.com>
2021-02-04 15:21:56 -0500

    fixed home page component mobile responsiveness & font size

--- next/components/Intro.js
+++ next/components/Intro.js
@@ -1,19 +1,23 @@
+import Link from 'next/link'
+
 export default function Intro() {
-  return (
-    <div className="intro">
-      <h1>We are Whitmire's Furniture</h1>
-      <h4>A family tradition since 1947</h4>
-      <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit.”
-      </p>
-      <button>About Us</button>
-    </div>
-  );
+	return (
+		<div className='intro'>
+			<h1>We are Whitmire's Furniture</h1>
+			<h4>A family tradition since 1947</h4>
+			<p>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+				adipiscing elit.”
+			</p>
+			<Link href='/about'>
+				<button>About Us</button>
+			</Link>
+		</div>
+	)
 }
