42cb0d81bbf3f9abe3240da62bd723c0d632c4e7
james <jguest777@gmail.com>
2021-02-05 10:50:46 -0500

    hacked menu ref for functionality, need to refactor

--- next/components/Header.js
+++ next/components/Header.js
@@ -1,73 +1,73 @@
-import { useState } from 'react';
-import { useTransition, animated } from 'react-spring';
-import Link from 'next/link';
-import SubMenu from './SubMenu';
-import Menu from './Menu';
+import { useState } from 'react'
+import { useTransition, animated } from 'react-spring'
+import Link from 'next/link'
+// import SubMenu from './SubMenu';
+import Menu from './Menu'
 
 export default function Header({ toggleOverlay, setToggleOverlay }) {
-  const [toggleMenu, setToggleMenu] = useState(false);
+	const [toggleMenu, setToggleMenu] = useState(false)
 
-  const transitions = useTransition(toggleMenu, null, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-    config: { duration: 200 },
-  });
+	const transitions = useTransition(toggleMenu, null, {
+		from: { opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
+		config: { duration: 200 }
+	})
 
-  const handleToggleMenu = () => {
-    setToggleMenu(!toggleMenu);
-    setToggleOverlay(!toggleOverlay);
-  };
+	const handleToggleMenu = () => {
+		setToggleMenu(!toggleMenu)
+		setToggleOverlay(!toggleOverlay)
+	}
 
-  return (
-    <header className="header">
-      <div className="header__logo">
-        <Link href="/">
-          <a>
-            <img src={require('../public/images/logo.png?webp')} />
-          </a>
-        </Link>
-      </div>
-      <nav>
-        {/* Mobile Icons & Burger Menu */}
-        {/* <SubMenu /> */}
-        <button
-          onClick={handleToggleMenu}
-          className={`header__burger ${toggleMenu && 'open'} hide-desktop`}
-        >
-          <div />
-          <div />
-          <div />
-        </button>
+	return (
+		<header className='header'>
+			<div className='header__logo'>
+				<Link href='/'>
+					<a>
+						<img src={require('../public/images/logo.png?webp')} />
+					</a>
+				</Link>
+			</div>
+			<nav>
+				{/* Mobile Icons & Burger Menu */}
+				{/* <SubMenu /> */}
+				<button
+					onClick={handleToggleMenu}
+					className={`header__burger ${toggleMenu && 'open'} hide-desktop`}
+				>
+					<div />
+					<div />
+					<div />
+				</button>
 
-        {/* Desktop Links */}
-        <div className="header__links hide-mobile">
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/about">
-            <a>Our Team</a>
-          </Link>
-          <Link href="/brands">
-            <a>Our Brands</a>
-          </Link>
-          <Link href="/contact">
-            <a>Contact Us</a>
-          </Link>
-        </div>
-      </nav>
-      {transitions.map(
-        ({ item, key, props }) =>
-          item && (
-            <animated.div key={key} style={props} className="header__menu">
-              <Menu
-                toggleMenu={toggleMenu}
-                setToggleMenu={setToggleMenu}
-                setToggleOverlay={setToggleOverlay}
-              />
-            </animated.div>
-          )
-      )}
-    </header>
-  );
+				{/* Desktop Links */}
+				<div className='header__links hide-mobile'>
+					<Link href='/'>
+						<a>Home</a>
+					</Link>
+					<Link href='/about'>
+						<a>Our Team</a>
+					</Link>
+					<Link href='/brands'>
+						<a>Our Brands</a>
+					</Link>
+					<Link href='/contact'>
+						<a>Contact Us</a>
+					</Link>
+				</div>
+			</nav>
+			{transitions.map(
+				({ item, key, props }) =>
+					item && (
+						<animated.div key={key} style={props} className='header__menu'>
+							<Menu
+								toggleMenu={toggleMenu}
+								setToggleMenu={setToggleMenu}
+								setToggleOverlay={setToggleOverlay}
+							/>
+						</animated.div>
+					)
+			)}
+		</header>
+	)
 }
--- next/components/Menu.js
+++ next/components/Menu.js
@@ -1,35 +1,43 @@
-import { useEffect, useRef } from 'react';
-import Link from 'next/link';
+import { useEffect, useRef } from 'react'
+import Link from 'next/link'
+
+// TODO: Very hacky ref solution, refactor!
+// Track multiple refs (links) during click event
 
 export default function Menu({ toggleMenu, setToggleMenu, setToggleOverlay }) {
-  const menuRef = useRef();
+	const linkRef = useRef()
+	const menuRef = useRef()
 
-  useEffect(() => {
-    if (!toggleMenu) return;
-    function handleCLick(e) {
-      if (menuRef.current && !menuRef.current.contains(e.target)) {
-        setToggleMenu(false);
-        setToggleOverlay(false);
-      }
-    }
-    window.addEventListener('click', handleCLick);
-    return () => window.removeEventListener('click', handleCLick);
-  }, [toggleMenu]);
+	useEffect(() => {
+		if (!toggleMenu) return
+		function handleCLick(e) {
+			if (
+				(menuRef.current && !menuRef.current.contains(e.target)) ||
+				linkRef.current.contains(e.target) ||
+				!linkRef.current.contains(e.target)
+			) {
+				setToggleMenu(false)
+				setToggleOverlay(false)
+			}
+		}
+		window.addEventListener('click', handleCLick)
+		return () => window.removeEventListener('click', handleCLick)
+	}, [toggleMenu])
 
-  return (
-    <>
-      <Link href="/">
-        <a ref={menuRef}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a ref={menuRef}>Our Team</a>
-      </Link>
-      <Link href="/brands">
-        <a ref={menuRef}>Our Brands</a>
-      </Link>
-      <Link href="/contact">
-        <a ref={menuRef}>Contact Us</a>
-      </Link>
-    </>
-  );
+	return (
+		<div ref={menuRef}>
+			<Link href='/'>
+				<a ref={linkRef}>Home</a>
+			</Link>
+			<Link href='/about'>
+				<a ref={linkRef}>Our Team</a>
+			</Link>
+			<Link href='/brands'>
+				<a ref={linkRef}>Our Brands</a>
+			</Link>
+			<Link href='/contact'>
+				<a ref={linkRef}>Contact Us</a>
+			</Link>
+		</div>
+	)
 }
